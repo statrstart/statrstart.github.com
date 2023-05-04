90bc10890814179a2e006932270011bcfc514298
statrstart <aishikoto@gmail.com>
2023-05-04 09:43:52 +0900

    Add files via upload

---
+++ source/data/WEOcountry.R
@@ -0,0 +1,238 @@
+WEOcountry<-read.csv(text=
+"Name,iso2c,iso3c,WEO,continent,region,destination_lon,destination_lat,capital.lon,capital.lat
+Andorra,AD,AND,171,Europe,Europe & Central Asia,1.5,42.5,1.52,42.5
+United Arab Emirates,AE,ARE,466,Asia,Middle East & North Africa,54,24,54.37,24.47
+Afghanistan,AF,AFG,512,Asia,South Asia,65,33,69.18,34.52
+Antigua & Barbuda,AG,ATG,311,Americas,Latin America & Caribbean,-61.8,17.05,-61.85,17.12
+Anguilla,AI,AIA,312,Americas,Latin America & Caribbean,-63.05,18.217,-63.05,18.22
+Albania,AL,ALB,914,Europe,Europe & Central Asia,20,41,19.82,41.32
+Armenia,AM,ARM,911,Asia,Europe & Central Asia,45,40.25,44.5,40.17
+Angola,AO,AGO,614,Africa,Sub-Saharan Africa,18.5,-12.5,13.22,-8.83
+Argentina,AR,ARG,213,Americas,Latin America & Caribbean,-64,-34,-58.67,-34.58
+American Samoa,AS,ASM,859,Oceania,East Asia & Pacific,-170.73,-14.318,-170.7,-14.27
+Austria,AT,AUT,122,Europe,Europe & Central Asia,13.333,47.333,16.37,48.2
+Australia,AU,AUS,193,Oceania,East Asia & Pacific,135,-25,149.13,-35.27
+Aruba,AW,ABW,314,Americas,Latin America & Caribbean,-69.967,12.5,-70.03,12.52
+Landskapet Åland,AX,,,,,19.952,60.198,,
+Azerbaijan,AZ,AZE,912,Asia,Europe & Central Asia,47.5,40.5,49.87,40.38
+Bosnia & Herzegovina,BA,BIH,963,Europe,Europe & Central Asia,17.786,44.169,18.42,43.87
+Barbados,BB,BRB,316,Americas,Latin America & Caribbean,-59.533,13.167,-59.62,13.1
+Bangladesh,BD,BGD,513,Asia,South Asia,90,24,90.4,23.72
+Belgium,BE,BEL,124,Europe,Europe & Central Asia,4,50.833,4.33,50.83
+Burkina Faso,BF,BFA,748,Africa,Sub-Saharan Africa,-1.74,12.278,-1.52,12.37
+Bulgaria,BG,BGR,918,Europe,Europe & Central Asia,25.25,42.667,23.32,42.68
+Bahrain,BH,BHR,419,Asia,Middle East & North Africa,50.55,26.033,50.57,26.23
+Burundi,BI,BDI,618,Africa,Sub-Saharan Africa,30,-3.5,29.35,-3.37
+Benin,BJ,BEN,638,Africa,Sub-Saharan Africa,2.25,9.5,2.62,6.48
+Bermuda,BM,BMU,319,Americas,North America,-64.75,32.333,-64.78,32.28
+Brunei,BN,BRN,516,Asia,East Asia & Pacific,114.667,4.5,114.93,4.88
+Bolivia,BO,BOL,218,Americas,Latin America & Caribbean,-65,-17,-68.15,-16.5
+Brazil,BR,BRA,223,Americas,Latin America & Caribbean,-55,-10,-47.92,-15.78
+Bahamas,BS,BHS,313,Americas,Latin America & Caribbean,-76,24.25,-77.35,25.08
+Bhutan,BT,BTN,514,Asia,South Asia,90.5,27.5,89.63,27.47
+Botswana,BW,BWA,616,Africa,Sub-Saharan Africa,24,-22,25.9,-24.63
+Belarus,BY,BLR,913,Europe,Europe & Central Asia,28,53,27.57,53.9
+Belize,BZ,BLZ,339,Americas,Latin America & Caribbean,-88.75,17.25,-88.77,17.25
+Canada,CA,CAN,156,Americas,North America,-96,60,-75.7,45.42
+Cocos (Keeling) Islands,CC,,,,,96.833,-12.5,96.83,-12.17
+Congo - Kinshasa,CD,COD,636,Africa,Sub-Saharan Africa,25,0.017,15.3,-4.32
+Central African Republic,CF,CAF,626,Africa,Sub-Saharan Africa,21,7,18.58,4.37
+Congo - Brazzaville,CG,COG,634,Africa,Sub-Saharan Africa,15,-1,15.28,-4.25
+Switzerland,CH,CHE,146,Europe,Europe & Central Asia,8,47,7.47,46.92
+Côte d’Ivoire,CI,CIV,662,Africa,Sub-Saharan Africa,-5.556,7.632,-5.27,6.82
+Cook Islands,CK,COK,815,Oceania,East Asia & Pacific,-159.767,-21.233,-159.77,-21.2
+Chile,CL,CHL,228,Americas,Latin America & Caribbean,-71,-30,-70.67,-33.45
+Cameroon,CM,CMR,622,Africa,Sub-Saharan Africa,12,6,11.52,3.87
+China,CN,CHN,924,Asia,East Asia & Pacific,105,35,116.38,39.92
+Colombia,CO,COL,233,Americas,Latin America & Caribbean,-74.817,3.4,-74.08,4.6
+Costa Rica,CR,CRI,238,Americas,Latin America & Caribbean,-83.946,9.971,-84.08,9.93
+Cuba,CU,CUB,928,Americas,Latin America & Caribbean,-81.3,22.733,-82.35,23.12
+Cape Verde,CV,CPV,624,Africa,Sub-Saharan Africa,-24,16,-23.52,14.92
+Territory of Christmas Island,CX,,,,,105.704,-10.444,105.72,-10.42
+Cyprus,CY,CYP,423,Asia,Europe & Central Asia,33,35,33.37,35.17
+Czechia,CZ,CZE,935,Europe,Europe & Central Asia,15,49.75,14.47,50.08
+Germany,DE,DEU,134,Europe,Europe & Central Asia,10.5,51.5,13.4,52.52
+Djibouti,DJ,DJI,611,Africa,Middle East & North Africa,43.167,11.55,43.15,11.58
+Denmark,DK,DNK,128,Europe,Europe & Central Asia,10,56,12.58,55.67
+Dominica,DM,DMA,321,Americas,Latin America & Caribbean,-61.35,15.433,-61.4,15.3
+Dominican Republic,DO,DOM,243,Americas,Latin America & Caribbean,-70.667,19,-69.9,18.47
+Algeria,DZ,DZA,612,Africa,Middle East & North Africa,3,28,3.05,36.75
+Ecuador,EC,ECU,248,Americas,Latin America & Caribbean,-78.25,-1.25,-78.5,-0.22
+Estonia,EE,EST,939,Europe,Europe & Central Asia,26,59,24.72,59.43
+Egypt,EG,EGY,469,Africa,Middle East & North Africa,30,27,31.25,30.05
+Eritrea,ER,ERI,643,Africa,Sub-Saharan Africa,39,15,38.93,15.33
+Spain,ES,ESP,184,Europe,Europe & Central Asia,-4,40,-3.68,40.4
+Ethiopia,ET,ETH,644,Africa,Sub-Saharan Africa,39.5,9,38.7,9.03
+Finland,FI,FIN,172,Europe,Europe & Central Asia,26,64,24.93,60.17
+Fiji,FJ,FJI,819,Oceania,East Asia & Pacific,178,-18,178.42,-18.13
+Falkland Islands,FK,FLK,323,Americas,Latin America & Caribbean,-58.694,-51.665,-57.85,-51.7
+Micronesia (Federated States of),FM,FSM,868,Oceania,East Asia & Pacific,158.235,6.883,158.15,6.92
+Faroe Islands,FO,FRO,816,Europe,Europe & Central Asia,-7,62,-6.77,62
+France,FR,FRA,132,Europe,Europe & Central Asia,2,46,2.33,48.87
+Gabon,GA,GAB,646,Africa,Sub-Saharan Africa,11.75,-1,9.45,0.38
+United Kingdom,GB,GBR,112,Europe,Europe & Central Asia,-2,54,-0.08,51.5
+Grenada,GD,GRD,328,Americas,Latin America & Caribbean,-61.667,12.117,-61.75,12.05
+Georgia,GE,GEO,915,Asia,Europe & Central Asia,43.5,42,44.83,41.68
+Bailiwick of Guernsey,GG,,,,,-2.583,49.467,-2.53,49.45
+Ghana,GH,GHA,652,Africa,Sub-Saharan Africa,-1,8,-0.22,5.55
+Gibraltar,GI,GIB,823,Europe,Europe & Central Asia,-5.35,36.133,-5.35,36.13
+Greenland,GL,GRL,326,Americas,Europe & Central Asia,-40,72,-51.75,64.18
+Gambia,GM,GMB,648,Africa,Sub-Saharan Africa,-15.386,13.453,-16.57,13.45
+Guinea,GN,GIN,656,Africa,Sub-Saharan Africa,-10,5,-13.7,9.5
+Equatorial Guinea,GQ,GNQ,642,Africa,Sub-Saharan Africa,10,2,8.78,3.75
+Greece,GR,GRC,174,Europe,Europe & Central Asia,22,39,23.73,37.98
+Guatemala,GT,GTM,258,Americas,Latin America & Caribbean,-90.367,14.633,-90.52,14.62
+Guam,GU,GUM,829,Oceania,East Asia & Pacific,144.707,13.385,144.73,13.47
+Guinea-Bissau,GW,GNB,654,Africa,Sub-Saharan Africa,-15,12,-15.58,11.85
+Guyana,GY,GUY,336,Americas,Latin America & Caribbean,-59,5,-58.15,6.8
+Hong Kong SAR,HK,HKG,532,,,114,22.5,,
+Honduras,HN,HND,268,Americas,Latin America & Caribbean,-86.5,15,-87.22,14.1
+Croatia,HR,HRV,960,Europe,Europe & Central Asia,15.5,45.167,16,45.8
+Haiti,HT,HTI,263,Americas,Latin America & Caribbean,-72.417,19,-72.33,18.53
+Hungary,HU,HUN,944,Europe,Europe & Central Asia,20,47,19.08,47.5
+Indonesia,ID,IDN,536,Asia,East Asia & Pacific,120,0,106.82,-6.17
+Ireland,IE,IRL,178,Europe,Europe & Central Asia,-8,53.333,-6.23,53.32
+Israel,IL,ISR,436,Asia,Middle East & North Africa,34.75,31.5,35.23,31.77
+Isle of Man,IM,,,,,-4.527,54.229,-4.48,54.15
+India,IN,IND,534,Asia,South Asia,77,20,77.2,28.6
+Iraq,IQ,IRQ,433,Asia,Middle East & North Africa,44,33,44.4,33.33
+Iran,IR,IRN,429,Asia,Middle East & North Africa,53,32,51.42,35.7
+Iceland,IS,ISL,176,Europe,Europe & Central Asia,-18,65,-21.95,64.15
+Italy,IT,ITA,136,Europe,Europe & Central Asia,12.833,42.833,12.48,41.9
+Bailiwick of Jersey,JE,,,,,-2.117,49.217,-2.1,49.18
+Jamaica,JM,JAM,343,Americas,Latin America & Caribbean,-77.25,18.167,-76.8,18
+Jordan,JO,JOR,439,Asia,Middle East & North Africa,35.557,31.763,35.93,31.95
+Japan,JP,JPN,158,Asia,East Asia & Pacific,138,36,139.75,35.68
+Kenya,KE,KEN,664,Africa,Sub-Saharan Africa,38,1,36.82,-1.28
+Kyrgyzstan,KG,KGZ,917,Asia,Europe & Central Asia,75,41.5,74.6,42.87
+Cambodia,KH,KHM,522,Asia,East Asia & Pacific,105,13,104.92,11.55
+Kiribati,KI,KIR,826,Oceania,East Asia & Pacific,-170,-5,173.03,1.35
+Comoros,KM,COM,632,Africa,Sub-Saharan Africa,43.833,-12,43.23,-11.7
+St. Kitts & Nevis,KN,KNA,361,Americas,Latin America & Caribbean,-62.75,17.333,-62.72,17.3
+North Korea,KP,PRK,,Asia,East Asia & Pacific,126.451,39.778,125.75,39.02
+South Korea,KR,KOR,542,Asia,East Asia & Pacific,128.103,36.504,126.98,37.55
+Kuwait,KW,KWT,443,Asia,Middle East & North Africa,47.75,29.5,47.97,29.37
+Cayman Islands,KY,CYM,377,Americas,Latin America & Caribbean,-81.198,19.314,-81.38,19.3
+Kazakhstan,KZ,KAZ,916,Asia,Europe & Central Asia,62,48,71.42,51.17
+Laos,LA,LAO,544,Asia,East Asia & Pacific,105,18,102.6,17.97
+Lebanon,LB,LBN,446,Asia,Middle East & North Africa,35.833,33.833,35.5,33.87
+St. Lucia,LC,LCA,362,Americas,Latin America & Caribbean,-60.969,13.898,-61,14
+Liechtenstein,LI,LIE,,,,9.533,47.167,9.52,47.13
+Sri Lanka,LK,LKA,524,Asia,South Asia,80.77,7.87,,
+Liberia,LR,LBR,668,Africa,Sub-Saharan Africa,-9.5,6.5,-10.8,6.3
+Lesotho,LS,LSO,666,Africa,Sub-Saharan Africa,28.25,-29.5,27.48,-29.32
+Lithuania,LT,LTU,946,Europe,Europe & Central Asia,24,56,25.32,54.68
+Luxembourg,LU,LUX,137,Europe,Europe & Central Asia,6.167,49.75,6.12,49.6
+Latvia,LV,LVA,941,Europe,Europe & Central Asia,25,57,24.1,56.95
+Libya,LY,LBY,672,Africa,Middle East & North Africa,17,25,13.17,32.88
+Morocco,MA,MAR,686,Africa,Middle East & North Africa,-6,32,-6.82,34.02
+Principauté de Monaco,MC,,,,,7.419,43.733,7.42,43.73
+Moldova,MD,MDA,921,Europe,Europe & Central Asia,28.583,47.25,28.85,47
+Montenegro,ME,MNE,943,Europe,Europe & Central Asia,19.333,42.5,19.27,42.43
+Madagascar,MG,MDG,674,Africa,Sub-Saharan Africa,47,-20,47.52,-18.92
+Marshall Islands,MH,MHL,867,Oceania,East Asia & Pacific,168,9,171.38,7.1
+North Macedonia,MK,MKD,962,Europe,Europe & Central Asia,21.75,41.667,21.43,42
+Mali,ML,MLI,678,Africa,Sub-Saharan Africa,-8.394,12.159,-8,12.65
+Myanmar (Burma),MM,MMR,518,Asia,East Asia & Pacific,96,21,96.15,16.8
+Mongolia,MN,MNG,948,Asia,East Asia & Pacific,105,45,106.92,47.92
+Northern Mariana Islands,MP,,,,,145.623,15.005,145.75,15.2
+Mauritania,MR,MRT,682,Africa,Sub-Saharan Africa,-10.5,20.25,-15.97,18.07
+Montserrat,MS,MSR,351,Americas,Latin America & Caribbean,-62.2,16.75,-62.22,16.7
+Malta,MT,MLT,181,Europe,Middle East & North Africa,14.417,35.917,14.5,35.88
+Mauritius,MU,MUS,684,Africa,Sub-Saharan Africa,57.55,-20.283,57.48,-20.15
+Maldives,MV,MDV,556,Asia,South Asia,73,3.2,73.5,4.17
+Malawi,MW,MWI,676,Africa,Sub-Saharan Africa,34,-13.5,33.78,-13.97
+Mexico,MX,MEX,273,Americas,Latin America & Caribbean,-105.796,30.562,-99.13,19.43
+Malaysia,MY,MYS,548,Asia,East Asia & Pacific,112.5,2.5,101.7,3.17
+Mozambique,MZ,MOZ,688,Africa,Sub-Saharan Africa,35,-18.25,32.58,-25.95
+Namibia,NA,NAM,728,Africa,Sub-Saharan Africa,17,-22,17.08,-22.57
+New Caledonia,NC,NCL,839,Oceania,East Asia & Pacific,165.5,-21.5,166.45,-22.27
+Niger,NE,NER,692,Africa,Sub-Saharan Africa,9.5,17,2.12,13.52
+Norfolk Island,NF,,,,,167.95,-29.033,167.97,-29.05
+Nigeria,NG,NGA,694,Africa,Sub-Saharan Africa,8,10,7.53,9.08
+Nicaragua,NI,NIC,278,Americas,Latin America & Caribbean,-85.833,11.433,-86.25,12.13
+Netherlands,NL,NLD,138,Europe,Europe & Central Asia,5.75,52.5,4.92,52.35
+Norway,NO,NOR,142,Europe,Europe & Central Asia,10,62,10.75,59.92
+Nepal,NP,NPL,558,Asia,South Asia,84,28,85.32,27.72
+Nauru,NR,NRU,836,,,166.8,-0.5,,
+Niue,NU,,,,,-169.867,-19.033,-169.92,-19.02
+New Zealand,NZ,NZL,196,Oceania,East Asia & Pacific,174,-41,174.78,-41.3
+Oman,OM,OMN,449,Asia,Middle East & North Africa,57.985,22.164,58.58,23.62
+Panama,PA,PAN,283,Americas,Latin America & Caribbean,-80,9,-79.53,8.97
+Peru,PE,PER,293,Americas,Latin America & Caribbean,-75.25,-10,-77.05,-12.05
+French Polynesia,PF,PYF,887,Oceania,East Asia & Pacific,-140,-15,-149.57,-17.53
+Papua New Guinea,PG,PNG,853,Oceania,East Asia & Pacific,143.459,-5.949,147.18,-9.45
+Philippines,PH,PHL,566,Asia,East Asia & Pacific,122,13,120.97,14.6
+Pakistan,PK,PAK,564,Asia,South Asia,70,30,73.05,33.68
+Poland,PL,POL,964,Europe,Europe & Central Asia,20,52,21,52.25
+St. Pierre & Miquelon,PM,SPM,363,Americas,North America,-56.333,46.833,-56.18,46.77
+Pitcairn Henderson Ducie and Oeno Islands,PN,,,,,-128.316,-24.366,-130.08,-25.07
+Puerto Rico,PR,PRI,359,,,-66.59,18.22,,
+Portugal,PT,PRT,182,Europe,Europe & Central Asia,-8,39.5,-9.13,38.72
+Palau,PW,PLW,565,Oceania,East Asia & Pacific,134,6,134.63,7.48
+Paraguay,PY,PRY,288,Americas,Latin America & Caribbean,-58,-23,-57.67,-25.27
+Qatar,QA,QAT,453,Asia,Middle East & North Africa,51.25,25.5,51.53,25.28
+Romania,RO,ROU,968,Europe,Europe & Central Asia,25,46,26.1,44.43
+Serbia,RS,SRB,942,Europe,Europe & Central Asia,21.066,43.139,20.5,44.83
+Russia,RU,RUS,922,Europe,Europe & Central Asia,100,60,37.6,55.75
+Rwanda,RW,RWA,714,Africa,Sub-Saharan Africa,30,-2,30.05,-1.95
+Saudi Arabia,SA,SAU,456,Asia,Middle East & North Africa,44.585,24.023,46.7,24.65
+Solomon Islands,SB,SLB,813,Oceania,East Asia & Pacific,159,-8,159.95,-9.43
+Seychelles,SC,SYC,718,Africa,Sub-Saharan Africa,55.474,-4.647,55.45,-4.62
+Sudan,SD,SDN,732,Africa,Sub-Saharan Africa,30,16,32.53,15.6
+Sweden,SE,SWE,144,Europe,Europe & Central Asia,15,62,18.05,59.33
+Singapore,SG,SGP,576,Asia,East Asia & Pacific,103.85,1.283,103.85,1.28
+St. Helena,SH,SHN,856,Africa,,-5.71,-15.953,-5.72,-15.93
+Slovenia,SI,SVN,961,Europe,Europe & Central Asia,15,46.083,14.52,46.05
+Svalbard og Jan Mayen,SJ,,,,,20,78,15.63,78.22
+Slovakia,SK,SVK,936,Europe,Europe & Central Asia,19.5,48.667,17.12,48.15
+Sierra Leone,SL,SLE,724,Africa,Sub-Saharan Africa,-11.5,8.5,-13.23,8.48
+San Marino,SM,SMR,135,Europe,Europe & Central Asia,12.417,43.933,12.42,43.93
+Senegal,SN,SEN,722,Africa,Sub-Saharan Africa,-14.25,14.5,-17.63,14.73
+Somalia,SO,SOM,726,Africa,Sub-Saharan Africa,43.34,11.635,45.33,2.07
+Suriname,SR,SUR,366,Americas,Latin America & Caribbean,-56,4,-55.17,5.83
+South Sudan,SS,SSD,733,Africa,Sub-Saharan Africa,30,8,31.62,4.85
+São Tomé & Príncipe,ST,STP,716,Africa,Sub-Saharan Africa,7,1,6.73,0.33
+El Salvador,SV,SLV,253,Americas,Latin America & Caribbean,-88.866,13.736,-89.2,13.7
+Syria,SY,SYR,463,Asia,Middle East & North Africa,38,35,36.3,33.5
+Eswatini,SZ,SWZ,734,Africa,Sub-Saharan Africa,31.5,-26.5,31.13,-26.32
+Turks and Caicos Islands,TC,,,,,-71.95,21.902,-71.13,21.47
+Chad,TD,TCD,628,Africa,Sub-Saharan Africa,19,15,15.03,12.1
+Togo,TG,TGO,742,Africa,Sub-Saharan Africa,1.167,8,1.22,6.12
+Thailand,TH,THA,578,Asia,East Asia & Pacific,101,15.5,100.52,13.75
+Tajikistan,TJ,TJK,923,Asia,Europe & Central Asia,71,39,68.77,38.55
+Timor-Leste,TL,TLS,537,Asia,East Asia & Pacific,126,-8.583,125.6,-8.58
+Turkmenistan,TM,TKM,925,Asia,Europe & Central Asia,59.667,39.75,58.38,37.95
+Tunisia,TN,TUN,744,Africa,Middle East & North Africa,9,34,10.18,36.8
+Tonga,TO,TON,866,Oceania,East Asia & Pacific,-175,-20,-175.2,-21.13
+Turkey,TR,TUR,186,Asia,Europe & Central Asia,35,39,32.87,39.93
+Trinidad & Tobago,TT,TTO,369,Americas,Latin America & Caribbean,-61,11,-61.52,10.65
+Tuvalu,TV,TUV,869,,,178,-8,179.22,-8.52
+Taiwan,TW,TWN,528,Asia,East Asia & Pacific,120.684,24.147,121.52,25.03
+Tanzania,TZ,TZA,738,Africa,Sub-Saharan Africa,35,-6,39.28,-6.8
+Ukraine,UA,UKR,926,Europe,Europe & Central Asia,32,49,30.52,50.43
+Uganda,UG,UGA,746,Africa,Sub-Saharan Africa,32.5,1.25,32.55,0.32
+United States,US,USA,111,Americas,North America,-97,38,-77.03,38.88
+Uruguay,UY,URY,298,Americas,Latin America & Caribbean,-56,-33,-56.17,-34.85
+Uzbekistan,UZ,UZB,927,,,64.58,41.38,,
+St. Vincent & Grenadines,VC,VCT,364,Americas,Latin America & Caribbean,-61.194,13.248,-61.22,13.13
+Venezuela,VE,VEN,299,Americas,Latin America & Caribbean,-66,8,-66.87,10.48
+British Virgin Islands,VG,,,,,-64.39,18.483,-64.62,18.42
+Vietnam,VN,VNM,582,Asia,East Asia & Pacific,107.833,16.167,105.85,21.03
+Vanuatu,VU,VUT,846,Oceania,East Asia & Pacific,167,-16,168.32,-17.73
+Wallis-et-Futuna,WF,,,,,-176.2,-13.3,-171.93,-13.95
+Samoa,WS,WSM,862,Oceania,East Asia & Pacific,-172.133,-13.8,-171.77,-13.82
+Yemen,YE,YEM,474,Asia,Middle East & North Africa,47.5,15.5,44.2,15.35
+South Africa,ZA,ZAF,199,Africa,Sub-Saharan Africa,23.121,-30.558,28.22,-25.7
+Zambia,ZM,ZMB,754,Africa,Sub-Saharan Africa,28.5,-14.333,28.28,-15.42
+Zimbabwe,ZW,ZWE,698,Africa,Sub-Saharan Africa,30.732,-20.129,31.03,-17.82
+Kosovo,XK,,967,,,21,42.57,,
+West Bank and Gaza,PS,,487,,,35.4,32,,
+Macao SAR,MO,,546,,,113.54,22.2,,
+Curacao,CW,CUW,354,Americas,Latin America & Caribbean,-68.955,12.1558,,
+French Guiana,GF,GUF,333,Americas,Latin America & Caribbean,-53.241,3.924,,
+Guadeloupe,GP,GLP,329,Americas,Latin America & Caribbean,-61.441,16.286,,
+Saint Martin (French part),MF,MAF,352,Americas,Latin America & Caribbean,-63.041,18.094,-63.08,18.07
+Martinique,MQ,MTQ,349,Americas,Latin America & Caribbean,-61.021,14.653,,
+Reunion,RE,REU,696,Africa,,55.6,-21.1,,"  ,
+colClasses=c(rep("character",6),rep("numeric",4)),na.strings="" )
